b7dfcda4ee692b8676435ecb8e10a67a0bc7444f
Martin Emde <martin.emde@gmail.com>
2008-10-02 12:47:18 -0700

    Clean up the code a little (and add RDoc documentation)

--- lib/bg_secure.rb
+++ lib/bg_secure.rb
@@ -1,6 +1,31 @@
 require 'md5'
 
 module BgSecure
+  # Create secure urls for Bit Gravity by passing the url, your Bit Gravity
+  # shared secret key, and an optional options hash.
+  #
+  # * url - Either a full url (http://example.com/path/file.ext) or a path
+  # (/path/file.ext) to the secure asset. The response will be returned the
+  # same as it was sent (with or without the host). A document relative
+  # path will cause an ArgumentError.
+  # * secret - Your shared secret key from BitGravity.
+  # * options - An optional hash of options (explained below).
+  #
+  # Valid options:
+  #
+  # * :expires - An object responding to :to_time, or and integer for seconds
+  # since UTC Epoch. If this option is not passed, evaluates to false, or if 0
+  # is passed, the url will not expire.
+  # * :unlock - If set to true, will override any country based blocking. This
+  # option takes precedence over :allowed and :disallowed so please pass only
+  # one of these options.
+  # * :allowed - Either an array of countries ['US', 'CA'] or a string "US,CA"
+  # used to indicate allowed countries. This option will override :disallowed
+  # and be overridden by :unlock so please pass only one of these options.
+  # * :disallowed - Either an array of countries ['US', 'CA'] or a string
+  # "US,CA" used to indicate disallowed countries. This option will be
+  # overridden by :unlock or :allowed so please pass only one of these options.
+  #
   def self.url_for(url, secret, options = {})
     uri = URI.parse(url)
     path = uri.path
@@ -9,23 +34,16 @@ module BgSecure
       raise ArgumentError, "Path [#{path}] is invalid for generating a BitGravity secure URL"
     end
 
-    timeout = options[:expires] || 0
-    timeout = timeout.to_time.utc if timeout.respond_to?(:to_time)
-
-    unless timeout.respond_to?(:to_i)
-      raise ArgumentError, "Invalid :expires option [#{timeout.inspect}] for BitGravity secure URL"
-    end
-
     # We just glom onto the path here because the order matters and there's
-    # only a couple options
-    path << "?e=#{timeout.to_i}"
+    # only a couple options to add.
+    path << "?e=#{timeout(options[:expires])}"
       
     if options[:unlock]
       path << '&g=1'
     elsif options[:allowed]
-      path << "&a=#{country_options_to_s(options[:allowed])}"
+      path << "&a=#{country_options(options[:allowed])}"
     elsif options[:disallowed]
-      path << "&d=#{country_options_to_s(options[:disallowed])}"
+      path << "&d=#{country_options(options[:disallowed])}"
     end
 
     path << "&h=#{MD5.hexdigest(secret + path)}"
@@ -40,12 +58,23 @@ module BgSecure
   end
 
   protected
-    def self.country_options_to_s(countries)
+    def self.timeout(expires)
+      timeout = expires || 0
+      timeout = timeout.to_time.utc if timeout.respond_to?(:to_time)
+
+      unless timeout.respond_to?(:to_i)
+        raise ArgumentError, "Invalid :expires option [#{timeout.inspect}] for BitGravity secure URL"
+      end
+
+      timeout.to_i.to_s
+    end
+
+    def self.country_options(countries)
       case countries
       when String
-        countries
+        countries.gsub(/ /,'').upcase
       when Array
-        countries.map { |country| country.to_s }.join(',')
+        countries.map { |country| country.to_s.upcase }.join(',')
       else
         raise ArgumentError, "Invalid :allowed/:disallowed option [#{options[:allowed]}] for BitGravit secure URL"
       end
